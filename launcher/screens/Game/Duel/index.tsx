4cb35d1dcaacf5fdec39ebe533f5fad2b6ad43e1
kayaba <kayaba.fluctlight@gmail.com>
2022-11-17 03:34:37 +0700

    upgrade game preview to rusty-bevy implementation

--- launcher/screens/Game/Duel/index.tsx
+++ launcher/screens/Game/Duel/index.tsx
@@ -6,10 +6,6 @@ import {
 	View,
 } from 'react-native';
 import { useRoute } from '@react-navigation/native';
-import { useSnapshot } from 'utils/hook';
-import { AccountState, accountState } from 'utils/state/account';
-
-import { useBridge } from './bridge';
 
 const initialLayout: LayoutRectangle = {
 	x: 0,
@@ -18,24 +14,23 @@ const initialLayout: LayoutRectangle = {
 	height: 0,
 };
 
-// const iFrameSrc = 'http://localhost:7456';
-const iFrameSrc = '/game/index.html';
+const iFrameSrc = '/murg/index.html';
 
-export const GameDuel: FC = (props) => {
-	const { profile } = useSnapshot<AccountState>(accountState);
+export const GameDuel: FC = () => {
 	const route = useRoute();
 	const params = route.params as { id: string };
 	const iFrameRef = useRef<HTMLIFrameElement>(null);
 	const [layout, setLayout] = useState<LayoutRectangle>(initialLayout);
 	const frameStyle = {
+		border: 'none',
 		width: layout.width,
 		height: layout.height,
 	};
+
 	const onContainerLayout = ({ nativeEvent }: LayoutChangeEvent) => {
 		setLayout(nativeEvent.layout);
+		console.log(params.id, '<-- id of the game');
 	};
-
-	useBridge(params?.id, iFrameRef);
 
 	return (
 		<View style={styles.container} onLayout={onContainerLayout}>
@@ -51,6 +46,6 @@ export default GameDuel;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		// backgroundColor: 'red',
+		backgroundColor: '#060404',
 	},
 });
