9a545339f7ba7e536cb1ac295c34ea28afda71d1
kayaba <fluctlight-kayaba@gmail.com>
2022-12-15 15:37:27 +0700

    minor position adjustment

--- game/assets/scripts/CardManager.ts
+++ game/assets/scripts/CardManager.ts
@@ -4,12 +4,5 @@ const { ccclass, property } = _decorator;
 
 @ccclass('CardManager')
 export class CardManager extends Component {
-	start(): void {
-		tween(this.node)
-			.to(3, { position: new Vec3(10, 10, 10) }, { easing: 'bounceInOut' })
-			.to(3, { position: new Vec3(0, 0, 0) }, { easing: 'elasticOut' })
-			.union()
-			.repeat(999)
-			.start();
-	}
+	start(): void {}
 }
