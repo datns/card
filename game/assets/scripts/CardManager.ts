f745d0ec4a7a60e22aa2b5139a68e470f3fa33c2
kayaba <fluctlight-kayaba@gmail.com>
2022-12-16 16:41:37 +0700

    Card Preview when hover Cards in hand

--- game/assets/scripts/CardManager.ts
+++ game/assets/scripts/CardManager.ts
@@ -1,11 +1,42 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Animation, Component, Node, UIOpacity } from 'cc';
 
 const { ccclass } = _decorator;
+const { EventType } = Node;
+
+interface Props {
+	animation?: Animation;
+	uiOpacity?: UIOpacity;
+	cardPreview?: Node;
+}
 
 @ccclass('CardManager')
 export class CardManager extends Component {
+	props: Props = {};
 	start(): void {
-		const boardNode = this.node.getChildByPath('Safe Zone') as Node;
-		console.log(boardNode);
+		this.props = {
+			animation: this.node.getComponent('cc.Animation') as Animation,
+			uiOpacity: this.node.getComponent('cc.UIOpacity') as UIOpacity,
+			cardPreview: this.node.parent.parent.getChildByPath('Card Preview'),
+		};
+
+		this.bindMouseEvents();
+	}
+
+	bindMouseEvents(): void {
+		this.node.on(EventType.MOUSE_ENTER, this.onMouseEnter.bind(this));
+		this.node.on(EventType.MOUSE_LEAVE, this.onMouseLeave.bind(this));
+	}
+
+	onMouseEnter(): void {
+		const { cardPreview, uiOpacity } = this.props;
+		uiOpacity.opacity = 50;
+		cardPreview.setPosition(0, -180);
+	}
+
+	onMouseLeave(): void {
+		const { cardPreview, uiOpacity } = this.props;
+		uiOpacity.opacity = 255;
+		cardPreview.setPosition(190, 740);
+		console.log('mouse leave');
 	}
 }
