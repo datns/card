79218aa56ea1e1f66823e19634e6fc82b1d5beeb
kayaba <fluctlight-kayaba@gmail.com>
2022-12-30 09:47:36 +0700

    put replayer util under Replay module

--- game/assets/scripts/replayer/index.ts
+++ game/assets/scripts/replayer/index.ts
@@ -1,6 +1,6 @@
 import Engine, { DuelCommandBundle } from '@metacraft/murg-engine';
+import isEqual from 'lodash.isequal';
 
-import { extractHistoryDiff } from '../util/replayer';
 import { system } from '../util/system';
 
 import { runInitialDraw } from './draw';
@@ -33,5 +33,39 @@ export const runCommandBundle = (bundle: DuelCommandBundle): void => {
 		});
 	} catch (e) {
 		console.log(e);
+	}
+};
+
+export interface HistoryDiff {
+	isConsist: boolean;
+	fragment: DuelCommandBundle[];
+}
+
+export const extractHistoryDiff = (
+	localHistory: DuelCommandBundle[],
+	remoteHistory: DuelCommandBundle[],
+): HistoryDiff => {
+	if (localHistory.length < remoteHistory.length) {
+		/* <- local behind remote, need to keep up by replay missing fragment */
+		const remoteHistoryFragment = remoteHistory.slice(0, localHistory.length);
+
+		return {
+			isConsist: isEqual(remoteHistoryFragment, localHistory),
+			fragment: remoteHistory.slice(localHistory.length),
+		};
+	} else if (remoteHistory.length > localHistory.length) {
+		/* <- local ahead remote */
+		const localHistoryFragment = localHistory.slice(0, remoteHistory.length);
+
+		return {
+			isConsist: isEqual(localHistoryFragment, remoteHistory),
+			fragment: [],
+		};
+	} else {
+		/* <- local have equal length as remote */
+		return {
+			isConsist: isEqual(localHistory, remoteHistory),
+			fragment: [],
+		};
 	}
 };
