34c58469d2af9eedbd8a5ca09fb38ac6d789625f
kayaba <fluctlight-kayaba@gmail.com>
2023-01-09 21:54:53 +0700

    dynamic Turn Controller labels

--- game/assets/scripts/TurnController.ts
+++ game/assets/scripts/TurnController.ts
@@ -58,18 +58,34 @@ export class TurnController extends Component {
 		const isSetupPhase = phase === DuelPhases.Setup;
 		const isMyPhase = phaseOf === system.playerIds.me;
 		const button = this.orb.getComponent(Button);
+		const color = Color.fromHEX(new Color(), isMyPhase ? '#4da7ea' : '#ee5846');
+		const white = Color.fromHEX(new Color(), '#ffffff');
 
-		button.interactable = isSetupPhase && isMyPhase;
+		if (isSetupPhase) {
+			button.interactable = true;
+			animateFade(this.playerTurnGlow, isMyPhase ? 255 : 0);
+			animateFade(this.enemyTurnGlow, isMyPhase ? 0 : 255);
+		} else {
+			button.interactable = false;
+			animateFade(this.playerTurnGlow, 0);
+			animateFade(this.enemyTurnGlow, 0);
+		}
 
-		animateFade(this.playerTurnGlow, isSetupPhase && isMyPhase ? 255 : 0);
-		animateFade(this.enemyTurnGlow, isSetupPhase && isMyPhase ? 0 : 255);
-		animateSwapLabel(
-			this.turnLabel,
-			isMyPhase ? 'end turn' : 'enemy turn',
-			isMyPhase
-				? Color.fromHEX(new Color(), '#4da7ea')
-				: Color.fromHEX(new Color(), '#ee5846'),
-		);
+		if (phase === DuelPhases.Draw) {
+			const label = system.duel.turn === 0 ? 'distribute' : 'draw';
+
+			animateSwapLabel(this.turnLabel, label, color);
+		} else if (phase === DuelPhases.Setup) {
+			const label = isMyPhase ? 'end turn' : 'enemy turn';
+
+			animateSwapLabel(this.turnLabel, label, color);
+		} else if (phase === DuelPhases.PreFight) {
+			animateSwapLabel(this.turnLabel, 'pre fight', white);
+		} else if (phase === DuelPhases.Fight) {
+			animateSwapLabel(this.turnLabel, 'fight', white);
+		} else if (phase === DuelPhases.PostFight) {
+			animateSwapLabel(this.turnLabel, 'post fight', white);
+		}
 	}
 
 	onMouseEnter(): void {
