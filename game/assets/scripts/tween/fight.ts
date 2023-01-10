2ba5605cecf2f117cf920c8f3559f4e83fdb6d17
kayaba <fluctlight-kayaba@gmail.com>
2023-01-10 14:50:08 +0700

    tweak Fight and Ribbon animation

--- game/assets/scripts/tween/fight.ts
+++ game/assets/scripts/tween/fight.ts
@@ -6,10 +6,8 @@ export const animateCardAttack = async (
 ): Promise<void> => {
 	const from = node.getPosition();
 	const isMovingUp = from.y < 0;
-	const fastSeed = 24;
+	const fastSeed = 12;
 	const fastDelta = isMovingUp ? -fastSeed : fastSeed;
-	const slowSeed = 5;
-	const slowDelta = isMovingUp ? -slowSeed : slowSeed;
 	const destSeed = isPlayerAttack ? 46 : 62;
 	const destDelta = isMovingUp ? -destSeed : destSeed;
 
@@ -20,13 +18,12 @@ export const animateCardAttack = async (
 				{ position: new Vec3(from.x, from.y + fastDelta, 0) },
 				{ easing: 'elasticInOut' },
 			)
-			.by(0.5, { position: new Vec3(0, slowDelta, 0) })
 			.to(
 				0.2,
 				{ position: new Vec3(from.x, destDelta, 0) },
 				{ easing: 'expoInOut' },
 			)
-			.to(1, { position: from }, { easing: 'expoInOut' })
+			.to(0.5, { position: from }, { easing: 'expoInOut' })
 			.call(resolve)
 			.start();
 	});
--- game/assets/scripts/tween/hud.ts
+++ game/assets/scripts/tween/hud.ts
@@ -1,23 +1,34 @@
-import { Label, tween, Vec3 } from 'cc';
+import { Label, tween, UIOpacity, Vec3 } from 'cc';
 
 import { system } from '../util/system';
 
 export const animateRibbonAppear = async (message: string): Promise<void> => {
+	const node = system.globalNodes.ribbonMessage;
+	const uiOpacity = node.getComponent(UIOpacity);
+
 	system.globalNodes.ribbonMessage
 		.getChildByPath('message')
 		.getComponent(Label).string = message;
 
 	return new Promise((resolve) => {
-		tween(system.globalNodes.ribbonMessage)
+		tween(node)
 			.set({
 				scale: new Vec3(0, 0, 1),
 				position: new Vec3(0, 25, 0),
 			})
 			.to(1, { scale: new Vec3(1, 1, 1) }, { easing: 'circInOut' })
 			.delay(2)
+			.call(() => {
+				tween(uiOpacity)
+					.to(0.5, { opacity: 0 })
+					.call(() => {
+						node.setPosition(new Vec3(9999, 0, 0));
+						uiOpacity.opacity = 255;
+						resolve();
+					})
+					.start();
+			})
 			.to(0.5, { scale: new Vec3(0, 0, 1) }, { easing: 'expoInOut' })
-			.set({ position: new Vec3(-1000, 0, 0) })
-			.call(() => resolve())
 			.start();
 	});
 };
